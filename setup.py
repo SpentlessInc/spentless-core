626f0223cb5962bcf23652750b551562d2443247
Oleksii Petrushynskyi <32573872+PetrushynskyiOleksii@users.noreply.github.com>
2020-07-18 13:19:39 +0300

    v0.5.2 (#5)
    Implement module to work with JWT.

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 NAME = "sws.core"
-VERSION = "0.3"
+VERSION = "0.5.2"
 AUTHOR = "Sheet Without Sheet"
 INSTALL_REQUIRES = [
     "aiohttp==3.6.2",
     "aioredis==1.3.1",
     "hiredis==1.0.1",
-    "asyncpg==0.20.1"
+    "asyncpg==0.20.1",
+    "pyjwt==1.7.1"
 ]
 
 
